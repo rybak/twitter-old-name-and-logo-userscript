c4e01bf9f7af5035822bc26c8d452078dfd56556
Andrei Rybak <rybak.a.v@gmail.com>
2024-05-17 14:47:21 +0200

    metadata: support new domain x.com
    Twitter migrated the website to the x.com domain.  Add metadata entry
    `@match` for the new domain.
    
    https://techau.com.au/goodbye-twitter-com-welcome-to-x-com/
    https://economictimes.indiatimes.com/news/international/business/twitters-domain-is-now-officially-x-com-elon-musk-tweets/articleshow/110198217.cms

--- twitter-old-name-and-logo.user.js
+++ twitter-old-name-and-logo.user.js
@@ -4,7 +4,7 @@
 // @name:nl        Twitter: oude naam en logo terugbrengen
 // @name:es        Twitter: recupera el nombre y el logotipo antiguos
 // @namespace      https://github.com/rybak
-// @version        28
+// @version        29
 // @description    Changes the logo, tab name, and naming of "tweets" on Twitter
 // @description:de Ändert das Logo, den Tab-Namen und die Benennung von „Tweets“ auf Twitter
 // @description:nl Wijzigt het logo, de tabbladnaam en de naamgeving van "tweets" op Twitter
@@ -12,6 +12,7 @@
 // @author         Andrei Rybak
 // @license        MIT
 // @match          https://twitter.com/*
+// @match          https://x.com/*
 // @icon           https://abs.twimg.com/favicons/twitter.2.ico
 // @grant          GM_addStyle
 // @run-at         document-body
