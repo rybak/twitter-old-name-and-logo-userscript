9a52d273826db0495377146e653b036cd8458efa
Andrei Rybak <rybak.a.v@gmail.com>
2024-05-20 14:43:43 +0200

    replaceLogoInHeader: update selector for light theme color
    <div> and <svg> tags seem to have been slightly rearranged -- the
    required CSS class didn't change, but it's now on the <svg> tag.

--- twitter-old-name-and-logo.user.js
+++ twitter-old-name-and-logo.user.js
@@ -4,7 +4,7 @@
 // @name:nl        Twitter: oude naam en logo terugbrengen
 // @name:es        Twitter: recupera el nombre y el logotipo antiguos
 // @namespace      https://github.com/rybak
-// @version        30.1
+// @version        30.2
 // @description    Changes the logo, tab name, and naming of "tweets" on Twitter
 // @description:de Ändert das Logo, den Tab-Namen und die Benennung von „Tweets“ auf Twitter
 // @description:nl Wijzigt het logo, de tabbladnaam en de naamgeving van "tweets" op Twitter
@@ -176,7 +176,7 @@
 			[data-testid="TopNavBar"] div > div > div > div > div > div > div > svg path {
 				d: path("M23.643 4.937c-.835.37-1.732.62-2.675.733.962-.576 1.7-1.49 2.048-2.578-.9.534-1.897.922-2.958 1.13-.85-.904-2.06-1.47-3.4-1.47-2.572 0-4.658 2.086-4.658 4.66 0 .364.042.718.12 1.06-3.873-.195-7.304-2.05-9.602-4.868-.4.69-.63 1.49-.63 2.342 0 1.616.823 3.043 2.072 3.878-.764-.025-1.482-.234-2.11-.583v.06c0 2.257 1.605 4.14 3.737 4.568-.392.106-.803.162-1.227.162-.3 0-.593-.028-.877-.082.593 1.85 2.313 3.198 4.352 3.234-1.595 1.25-3.604 1.995-5.786 1.995-.376 0-.747-.022-1.112-.065 2.062 1.323 4.51 2.093 7.14 2.093 8.57 0 13.255-7.098 13.255-13.254 0-.2-.005-.402-.014-.602.91-.658 1.7-1.477 2.323-2.41z");
 			}
-			h1 div.r-18jsvk2 > svg path,
+			h1 svg.r-18jsvk2 path,
 			div.r-6026j[data-testid="TopNavBar"] div > div > div > div > div > div > svg path {
 				fill: #1DA1F2;
 			}
